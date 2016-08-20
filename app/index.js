b46b94aa553a4e1e078ef26472aeb2146b8f9ff8
Ben Swennen <b_swennen@hotmail.com>
2016-08-20 22:05:42 +0200

    Add sentry w. raven

--- app/index.js
+++ app/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Raven from 'raven-js';
+
 import Routes from './config/routes';
+
+const sentryKey = '42e9691ddb0c4e3399b51369ee5001ee';
+const sentryApp = '93318';
+const sentryUrl = `https://${sentryKey}@app.getsentry.com/${sentryApp}`;
+
+Raven.config(sentryUrl, {
+  release: '1.0.0',
+}).install();
 
 ReactDOM.render(
   <Routes />,
